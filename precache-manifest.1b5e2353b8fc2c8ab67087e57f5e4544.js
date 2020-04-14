eef26dd3551fb04e9aa358d8e2cc43150345f86c
Navin420 <bhattarnaveen01@gmail.com>
2020-04-14 22:04:21 +0530

    Updates

---
+++ precache-manifest.1b5e2353b8fc2c8ab67087e57f5e4544.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51c6ea6b95802b9f5e70714d98bb5c5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8cf97992ab0d8a7430e1",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "39a6684101b658ebd56e",
+    "url": "/robofriends/static/css/main.55426e27.chunk.css"
+  },
+  {
+    "revision": "8cf97992ab0d8a7430e1",
+    "url": "/robofriends/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a6684101b658ebd56e",
+    "url": "/robofriends/static/js/main.ede2d5b5.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
